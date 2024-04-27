ac8c052c6f261e28cf23f3c577eeb56a3f991ff6
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2024-04-27 17:08:29 +0300

    Ui and gun

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject startPanel;
+    public GameObject menuPanel;
+    public Button startButton;
+
+    void Start()
+    {
+        // Başlangıç panelini aktif hale getir
+        startPanel.SetActive(true);
+
+        // Oyuna başla butonuna tıklama olayını dinle
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    // Oyuna başlama fonksiyonu
+    public void StartGame()
+    {
+        // Oyuna başlama işlemleri burada gerçekleştirilecek
+        Debug.Log("Oyun Başlatıldı!");
+
+        // Başlangıç panelini kapat
+        startPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+}
